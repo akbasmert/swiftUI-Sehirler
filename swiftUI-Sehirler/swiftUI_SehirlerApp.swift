754ff590d902e80df9742982045eddc98f168f9a
mert <mert.ma346@gmail.com>
2022-09-11 14:20:50 +0300

    Initial Commit

---
+++ swiftUI-Sehirler/swiftUI_SehirlerApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_SehirlerApp.swift
+//  swiftUI-Sehirler
+//
+//  Created by Mert AKBAŞ on 11.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_SehirlerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
