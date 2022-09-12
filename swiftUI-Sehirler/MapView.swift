934fd34be1b92d2789837a3be1818715a2a8ca6a
mert <mert.ma346@gmail.com>
2022-09-12 10:45:51 +0300

    mapKit için yeni kodlar yazıldı(IOS sürümü 14 ve altında destekliyor)

--- swiftUI-Sehirler/MapView.swift
+++ swiftUI-Sehirler/MapView.swift
@@ -9,15 +9,24 @@ import SwiftUI
 import MapKit
 
 
-struct MapView: View {
-    @State var region = MKCoordinateRegion(center: sehirlerDizisi[0].koordinatLokasyonu, span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
-    var body: some View {
-        Map(coordinateRegion: $region)// Bu kullanım IOS 14 ve üstünü kapsadığı için başka bir yötem kullanacağız.
+struct MapView: UIViewRepresentable {
+    
+    var coordinate : CLLocationCoordinate2D
+   
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        
+        let span = MKCoordinateSpan(latitudeDelta: 0.9, longitudeDelta: 0.9)
+       let region = MKCoordinateRegion(center: coordinate, span: span)
+        uiView.setRegion(region, animated: true)
     }
+    func makeUIView(context: Context) -> MKMapView {
+        MKMapView(frame: .zero)
+    }
+    
 }
 
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
-        MapView()
+        MapView(coordinate: sehirlerDizisi[0].koordinatLokasyonu)
     }
 }
